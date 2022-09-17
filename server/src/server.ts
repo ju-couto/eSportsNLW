584237b83e6166049a71fb981cea27b37657e040
jucouto <juliamartinscouto0403@gmail.com>
2022-09-17 10:18:26 -0300

    feat: server creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.get('/ads', (request, response) => {
- 
-    return response.json([
-        { id: 1, name : "Anuncio 1" },
-        { id: 2, name : "Anuncio 2" },
-        { id: 3, name : "Anuncio 3" },
-        { id: 4, name : "Anuncio 4" },
-        { id: 5, name : "Anuncio 5" },
+  return response.json([
+    {id: 1, name: 'Ad 1'},
+    {id: 2, name: 'Ad 2'},
+    {id: 3, name: 'Ad 3'},
+    {id: 4, name: 'Ad 4'},
 
-
-    ])
+  ]);
 })
 
-app.listen(3333)
+app.listen(3333);
